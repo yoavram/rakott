2cf05e8ab308b603d15a4cb7126de14255ee9ab0
Yoav Ram <yoav@yoavram.com>
2019-05-20 14:41:36 +0300

    fix bug

--- rakott/scripts.py
+++ rakott/scripts.py
@@ -3,4 +3,4 @@ from datetime import datetime
 def todaystr():
 	"""Return the current day as a string, for example 20MAY2019 for May 20th, 2019.
 	"""
-	return now.strftime('%d%b%Y').upper()
+	return datetime.now().strftime('%d%b%Y').upper()
